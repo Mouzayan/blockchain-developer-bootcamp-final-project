8510af5a63e4eee01da0f5a3a7b8077ac4a17cc1
Mouzayan <42501101+Mouzayan@users.noreply.github.com>
2021-11-08 21:28:19 -0500

    contract compiling

--- contracts/SupplyManagement.sol
+++ contracts/SupplyManagement.sol
@@ -6,13 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title Controlled Supply Chain
 /// @author Mouzayan Delbourgo 
 /// @notice You can use this contract for basic supply chain assurance and control
-
-contract SupplyManagement {
-    /// @notice Calculate tree age in years, rounded up, for live trees
-    /// @dev The Alexandr N. Tetearing algorithm could increase precision
-    /// @param rings The number of rings from dendrochronological sample
-    /// @return Age in years, rounded up for partial years
+///@dev Some functions will have the side effect of updating state
+contract SupplyManagement is Ownable {
+    /// @notice The account that deployed the contract
     address public owner = msg.sender;
+    /// @notice This is the amount of purchased items
     uint256 public skuCount;
 
     mapping(uint => Item) items;
@@ -33,6 +31,14 @@ contract SupplyManagement {
 
     event LogForSale(uint sku);
     event LogSold(uint sku);
+
+     /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
 
     modifier isOwner () {
       require (msg.sender == owner);
@@ -67,10 +73,14 @@ contract SupplyManagement {
       _;
     }
 
-     constructor() payable {
-      owner == payable(msg.sender);
+     constructor() {
+      owner == msg.sender;
     }
     
+    /// @notice Select item and its quantity to purchase
+    /// @param _name of item
+    /// @param _price of item
+    /// @return Boolean to confirm that item was added for purchase
     function addItem (string memory _name, uint _price) public returns (bool) {
 
       items[skuCount] = Item({
@@ -78,8 +88,8 @@ contract SupplyManagement {
         sku: skuCount,
         price: _price,
         state: Status.ForSale,
-        seller: msg.sender,
-        buyer: address(0)
+        seller: payable(msg.sender),
+        buyer: payable(address(0))
       });
 
       skuCount = skuCount + 1;
@@ -87,10 +97,8 @@ contract SupplyManagement {
       return true;
     }
 
-    /// @notice Returns the Status of the transaction. 
-    /// @param sku decalredQuantity desiredQuantity buyer
-    /// @return Updated Status.
-    /// @return Remaining available inventory.
+    /// @notice Complete your purchase
+    /// @param sku of item
     function buyItem(uint sku) public payable forSale(sku) paidEnough(items[sku].price) checkValue(sku) {
       items[sku].seller.transfer(items[sku].price);
       items[sku].buyer == msg.sender;
@@ -99,7 +107,14 @@ contract SupplyManagement {
       emit LogSold(sku);
     }
 
-
+   /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
 }
 
 contract myContract is Ownable {
