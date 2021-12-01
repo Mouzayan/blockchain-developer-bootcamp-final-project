44d4740fbeedd70d0ab4bf26f53ee99c3e7f490e
Mouzayan <42501101+Mouzayan@users.noreply.github.com>
2021-11-30 21:51:29 -0500

    fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.scss';
 export default function Navbar (props) {
   return (
     <div className="navbar">
-        <div><h1>commodity market</div></div>
+        <div><h1>commodity market</h1></div>
         <div to="/connect to metamask">connected to metamask</div>
         <div>your account address is: {props.account} </div>
     </div>
