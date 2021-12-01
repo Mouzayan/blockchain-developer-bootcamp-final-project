5707a0ab6008e84deb4db42cb9cd7802a0d0652a
Mouzayan <42501101+Mouzayan@users.noreply.github.com>
2021-11-30 19:18:49 -0500

    contract address

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
 const path = require("path");
-//  const HDWalletProvider = require('@truffle/hdwallet-provider');
-//  const infuraURL = 'https://rinkeby.infura.io/v3/636e4a5d833644d0951cf2b96b549b2f'
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const infuraURL = 'https://rinkeby.infura.io/v3/636e4a5d833644d0951cf2b96b549b2f'
 
-//  const fs = require('fs');
-//  const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -14,14 +14,14 @@ module.exports = {
       host: "127.0.0.1",
       port: "8545",
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,          // Rinkeby's network id
+      gas: 5500000,        
     }
-  },
 
-  // rinkeby: {
-  //     provider: () => new HDWalletProvider(mnemonic, infuraURL),
-  //     network_id: 4,          // Rinkeby's network id
-  //     gas: 5500000,        
-  //   },
+  },
 
   compilers: {
     solc: {
